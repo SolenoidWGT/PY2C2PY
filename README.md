33430f3c85bf375a7ac00b1629701b4d86f13f7b
wangguoteng.p <wangguoteng@sensetime.com>
2023-02-10 14:26:40 +0800

    polish

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # PY2C2PY
+
+## 紧耦合交互
+
+1. **Extending**：python通过import的方式调用C/C++可执行程序，这需要python和c之间使用一些胶水程序来约定彼此的数据交换格式，比如cpython。
+
+2. **Embedding**：C/C++调用python解释器，执行python脚本，这种方式往往更复杂，需要Boost.Python等库，开发成本比较大。
+
+
+## 松耦合交互
+1. 使用popen+stdout/file的方式进行交互，见sdtio目录。
+   
+2. 使用protobuf+gRPC的方式进行交互，见grpc目录。
--- grpc/build.sh
+++ grpc/build.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=//mnt/lustre/wangguoteng.p/local//cmake/build:/mnt/cache/wangguoteng.p/demo/grpc/protobuf/lib
+export LIBRARY_PATH=//mnt/lustre/wangguoteng.p/local//cmake/build:/mnt/cache/wangguoteng.p/demo/grpc/protobuf/lib
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/fping/sbin:/usr/local/fping/sbin:/mnt/cache/share/platform/env/miniconda3.7/bin:/mnt/cache/wangguoteng.p/code-server-4.6.0-linux-amd64/bin:/mnt/cache/share/git-2.0.5/bin/
+# -L/mnt/cache/wangguoteng.p/demo/grpc/protobuf/lib 
+/mnt/cache/share/gcc/gcc-7.5.0/bin/g++ -std=c++17 *.cc -o c_rpc \
+    -L/mnt/lustre/wangguoteng.p/local/grpc/cmake/build \
+    -L/mnt/lustre/wangguoteng.p/local/grpc/lib64/ \
+    -Wl,-rpath,/mnt/lustre/wangguoteng.p/local/grpc/lib64/  \
+    -lpthread -lprotobuf -lgrpc++ -labsl_synchronization \
+    -I/mnt/lustre/wangguoteng.p/grpc/grpc/third_party/abseil-cpp/ \
+    -I/mnt/lustre/wangguoteng.p/local/grpc/third_party/ \
+    -I/mnt/lustre/wangguoteng.p/local/grpc/include  \
+    -I/mnt/lustre/wangguoteng.p/local/grpc/third_party/protobuf/src/ \
+    -I.
 
 cd - && \
 /mnt/lustre/wangguoteng.p/local/grpc/bin/protoc --cpp_out=../c ./test.proto && \
